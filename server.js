9d76eba3e1e8c6980501615184e49546aea4500c
Rusty <144427972+cdrusty@users.noreply.github.com>
2023-09-08 16:43:15 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const math = require('mathjs');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'));
+
+
+app.get('/math/:equation', (req, res) => {
+    try {
+        const equation = req.params.equation;
+        const result = math.evaluate(equation, { pi: Math.PI });
+        const equationWithResult = `The result of the math equation ${equation} is: ${result}`;
+
+
+        // Create an HTML page with a header and Open Graph meta tags
+        const htmlResponse = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Math Equation Result</title>
+                <meta property="og:title" content="Math Equation Result">
+                <meta property="og:description" content="${equationWithResult}">
+            </head>
+            <body>
+                <h1>Math Equation Result</h1>
+                <div id="result">${equationWithResult}</div>
+            </body>
+            </html>
+        `;
+
+
+        // Set the Content-Type header to text/html
+        res.setHeader('Content-Type', 'text/html');
+        
+        // Send the HTML response
+        res.send(htmlResponse);
+    } catch (error) {
+        res.status(400).send(`Error: ${error.message}`);
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
