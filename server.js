31dd0eaf9ce4bed357ee5d908e254ddf124b9144
Rusty <144427972+cdrusty@users.noreply.github.com>
2023-09-08 16:55:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = express();
 const port = 3000;
 
 
-app.use(express.static('public'));
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/math/:equation', (req, res) => {
     try {
